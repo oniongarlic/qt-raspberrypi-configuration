84f468d8e20b606b933eaf6cf1cc316848dd3456
Kaj-Michael Lang <milang@tal.org>
2019-04-26 09:39:36 +0300

    Add sample configuration script

---
+++ build/build-mkspecs-linux-rpi-g++.sh
@@ -0,0 +1,25 @@
+PKG_CONFIG_LIBDIR=/usr/lib/arm-linux-gnueabihf/pkgconfig:/usr/share/pkgconfig \
+../qt-everywhere-src-5.12.3/configure \
+-platform linux-rpi-g++ \
+-v -optimized-tools \
+-opengl es2 -eglfs \
+-no-gtk \
+-opensource -confirm-license -release \
+-reduce-exports \
+-force-pkg-config \
+-nomake examples -no-compile-examples \
+-skip qtwayland \
+-skip qtwebengine \
+-skip qtscript \
+-no-feature-geoservices_mapboxgl \
+-qt-pcre \
+-no-pch \
+-ssl \
+-evdev \
+-system-freetype \
+-fontconfig \
+-glib \
+-sctp \
+-prefix /opt/Qt5.12 \
+-recheck-all \
+-qpa eglfs
